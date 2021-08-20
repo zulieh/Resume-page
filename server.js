7d1dc61b2bd8d4a033a025d46c1a3e1369d9e6a0
Zuleiha Dantijani <tzuleiha@gmail.com>
2021-08-20 20:25:19 +0100

    finish

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const emailHandler = require("./util/emailHandler.js")
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config();
 
 const app = express();
-
 app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static("src"));
 
 
-// Dev logging middleware
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
-
-app.route("/").get(function (req, res) {
-    res.sendFile(process.cwd() + "/public/index.html");
-  });
-
-app.use('/', (req, res) => {
-  res.send('Welcome to code evaluation api');
+app.route("/").get(function(req, res) {
+  res.sendFile(process.cwd() + "/src/index.html");
 });
 
+app.route("/sendmail").post(function(req, res){
+  const { name, email, subject, message } = req.body;
+  
+  let detail = subject+" from "+name
+  emailHandler(email, detail, message, message).catch(err => console.log(err))  
+  res.redirect("/success")
+});
+
+app.route("/success").get(function(req, res) {
+  res.send("Message Recieved!!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Welcome to resume app");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`server is running at port ${PORT}`));
-
--- util/emailHandler.js
+++ util/emailHandler.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config({ path: './config/config.env' });
+require('dotenv').config();
 
 
 
